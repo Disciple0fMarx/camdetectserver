9144dac956dcd232b733c28d0563b20f92163607
Dhya Eddine El Bahri <45290395+Disciple0fMarx@users.noreply.github.com>
2023-05-17 17:16:22 +0100

    Added a superuser (dhya) and REST and API apps.

--- camdetectserver/settings.py
+++ camdetectserver/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'camdetect_api',
 ]
 
 MIDDLEWARE = [
