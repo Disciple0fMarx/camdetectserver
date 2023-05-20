23a1716012b499bd8f632a163d1427fff3db6ef6
Dhya Eddine El Bahri <45290395+Disciple0fMarx@users.noreply.github.com>
2023-05-20 18:43:37 +0100

    Registered AIObject models.

--- camdetect_api/admin.py
+++ camdetect_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Thing, Face, LicensePlate
 
 # Register your models here.
+admin.site.register(Thing)
+admin.site.register(Face)
+admin.site.register(LicensePlate)
