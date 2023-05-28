a010d9d9737aa46b483e499cf488057f61f04db7
Dhya Eddine El Bahri <45290395+Disciple0fMarx@users.noreply.github.com>
2023-05-28 21:20:36 +0100

    Added MEDIA_ROOT and MEDIA_URL.

--- camdetectserver/urls.py
+++ camdetectserver/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from camdetect_api import urls as camdetect_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('camdetect/', include(camdetect_urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
